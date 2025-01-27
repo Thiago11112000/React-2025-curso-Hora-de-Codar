f7f1a718365319a85bcb8cecca8c285a4ad08338
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-01-27 14:29:29 -0300

    Eventos

--- 2_FUNDAMEN6TOS/fundamentos/src/App.jsx
+++ 2_FUNDAMEN6TOS/fundamentos/src/App.jsx
@@ -10,6 +10,7 @@ import { useState } from 'react'
 import './App.css'
 import TemplateExpressions from './components/TemplateExpressions'
 import MyComponent from './components/MyComponent'
+import Events from './components/Events'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
       <FirstComponent/>
       <TemplateExpressions />
       <MyComponent />
+      <Events/>
     </>
   )
 }
