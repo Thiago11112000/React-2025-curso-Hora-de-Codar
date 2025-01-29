22f66cd07934fbb88ac819aab027e87eb2363398
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-01-29 11:08:15 -0300

    Funções dentro de eventos - prática não recomendada

--- 2_FUNDAMEN6TOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMEN6TOS/fundamentos/src/components/Events.jsx
@@ -9,6 +9,19 @@ return (
 <div>
     <button onClick={handleMyEvent}>Clique aqui!</button>
 </div>
+<div>
+    <button onClick={() => console.log("CLicou!")}>
+    Clique aqui também!
+    </button>
+    <button onClick={()=>{
+        if(true){
+            console.log("Isso não deveria existir =)")
+        }
+    }}
+    >
+        Clica aqui, por favor.
+    </button>
+</div>
     </div>
 )
 };
