6270375790f0f6b871ddf6181c4562ccdc75d55f
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-01-29 13:12:13 -0300

    Função de renderização

--- 2_FUNDAMEN6TOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMEN6TOS/fundamentos/src/components/Events.jsx
@@ -3,7 +3,13 @@ const handleMyEvent = (e) => {
 console.log(e);
 console.log("Ativou o evento");
 };
-
+const renderSomething = (x) =>{
+    if (x){
+        return  <h1> Renderizando isso</h1>
+    }else  {
+        return <h1> Também posso renderizar isso!</h1>
+    }
+}
 return (
     <div>
 <div>
@@ -22,6 +28,8 @@ return (
         Clica aqui, por favor.
     </button>
 </div>
+    {renderSomething(true)}
+    {renderSomething(false)}
     </div>
 )
 };
