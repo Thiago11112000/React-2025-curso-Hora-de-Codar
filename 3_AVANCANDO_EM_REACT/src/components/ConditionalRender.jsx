82377826ead647942e1dd4445924b2d753203318
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-02-03 11:53:02 -0300

    IF ELSE ternário

--- 3_AVANCANDO_EM_REACT/src/components/ConditionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/src/components/ConditionalRender.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
-  const ConditionalRender= ()  =>{
-    const [x] = useState(false);
+const ConditionalRender = () => {
+  const [x] = useState(false);
+  const [name, setName] = useState("Matheus");
 
-
-    return( 
+  return (
     <div>
-     <h1>Isso Será exibido ?</h1>
-     {x &&  <p> Se x for true, sim!</p>}
-     {!x && <p> Agora x é false</p>}
+      <h1>Isso será exibido?</h1>
+      {x && <p>Se x for true, sim!</p>}
+      {!x && <p>Agora x é false</p>}
+      <h1>If ternário</h1>
+      {name === "João" ? (
+        <div>
+          <p>O nome é João</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
+      <button onClick={()=> setName("João")}>Clique aqui!</button>
     </div>
-    );
-}
-export default ConditionalRender
+  );
+};
+
+export default ConditionalRender;
