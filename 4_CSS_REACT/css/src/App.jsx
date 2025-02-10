b7e824964289462192f24ab1edbcfaee093cf63f
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-02-10 13:50:27 -0300

    Css dos componentes

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import './components/MyComponent.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 return <div className='App'>
-
+    {/*Css Global */}
   <h1>React com Css</h1>
+    {/*Css de componente */}
+  <MyComponent/>
+  <p> Este parágrafo é do App.jsx</p>
 </div>
 
   
---
+++ 4_CSS_REACT/css/src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+const MyComponent = () => {
+  return (
+    <div>
+    <h1> CSS de Componente</h1>
+    <p> Este é o parágrafo do componente</p>    
+    <p className="my-comp-paragraph">Este tb é do componente</p>
+     </div>
+  )
+}
+
+export default MyComponent
