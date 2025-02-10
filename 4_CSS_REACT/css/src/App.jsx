6abff8ba2dd12983d4d38b5d820d935e47891896
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-02-10 15:05:47 -0300

    Css Inline Dinâmico

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,27 +1,36 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import './components/MyComponent.css'
-import MyComponent from './components/MyComponent'
+import './components/MyComponent.css';
+import { useState } from 'react';
+import MyComponent from './components/MyComponent';
 
 function App() {
-return <div className='App'>
-    {/*Css Global */}
-  <h1>React com Css</h1>
-    {/*Css de componente */}
-  <MyComponent/>
-  <p> Este parágrafo é do App.jsx</p>
-  {/*Inline CSS */}
-  <p style={{color:'magenta', padding:"25px", borderTop: "2px solid red"}}> 
-    Este elemento foi estilizado de forma inline 
-  </p>
-  <p style={{color:'magenta', padding:"25px", borderTop: "2px solid red"}}> 
-    Este elemento foi estilizado de forma inline 
-  </p>
-</div>
+  const n = 15;
+  const [name] = useState("Matheus");
 
-  
+  return (
+    <div className='App'>
+      {/* Css Global */}
+      <h1>React com Css</h1>
+      {/* Css de componente */}
+      <MyComponent />
+      <p>Este parágrafo é do App.jsx</p>
+      {/* Inline CSS */}
+      <p style={{ color: 'magenta', padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      <p style={{ color: 'magenta', padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>Css dinâmico</h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>Css dinâmico</h2>
+      <h2 style={name === "Matheus" ? { color: "green", backgroundColor: "#000" } : null}>
+        Teste
+      </h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
