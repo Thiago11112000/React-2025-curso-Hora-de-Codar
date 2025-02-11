31103d1377471c2c9430c07babca66ab670a5477
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-02-11 10:38:24 -0300

    Label envolvendo input

--- 5_FORM_EM_REACT/form/src/Components/Myform.jsx
+++ 5_FORM_EM_REACT/form/src/Components/Myform.jsx
@@ -9,6 +9,12 @@ const Myform = () => {
             <label htmlFor="name">Nome:</label>
             <input type="text" name="name"  placeholder="Digite o seu nome"/>
         </div>
+        {/*2 Label envolvendo Input*/}
+        <label>
+            <span>E-mail</span>
+            <input type="email" name= "email" placeholder='Digite o seu e-mail'  />
+        </label>
+
         <input type="submit" value="Enviar" />
         </form> 
     </div>
