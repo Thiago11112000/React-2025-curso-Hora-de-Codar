79fb1cef576c91c992b20d5bc54933331992c272
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-02-13 10:41:35 -0300

    Ajustes iniciais

--- 6_SECRET_WORD/secretword/src/App.jsx
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-        <h2> Secret Word</h2>
-    </>
-  )
+    <div className="App">
+      <h2>Secret Word</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
