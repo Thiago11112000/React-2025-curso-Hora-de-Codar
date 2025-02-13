e96a1c3f99511b4734b6c90bf9bc4b92c931fb4b
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-02-13 14:51:37 -0300

    Finalizando tela inicial

--- 6_SECRET_WORD/secretword/src/App.jsx
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import StartScreen from './components/StartScreen';
 function App() {
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      <StartScreen/>
     </div>
   );
 }
