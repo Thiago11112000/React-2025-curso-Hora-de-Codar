e956e96803c3085c796e8779abe2aedd6b6bdf3d
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-06 15:22:34 -0300

    Configurando o React Router

---
+++ 7_REACT_ROUTER/reactrouter/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+// 1 - config react router
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+
+
+function App() {
+  return (
+   <div className="App">
+    <h1> React Router </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+    </BrowserRouter>
+   </div>
+  )
+}
+
+export default App
