1df94e5b03cfc0c4e16aa45800a9a34d6dd81410
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-08 08:04:47 -0300

    Carregando dados dinâmicos

--- 7_REACT_ROUTER/reactrouter/src/pages/Product.jsx
+++ 7_REACT_ROUTER/reactrouter/src/pages/Product.jsx
@@ -1,13 +1,27 @@
 import React from "react"
-import { useParams
-
- } from "react-router-dom"
+import { data, useParams} from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
 const Product = () => {
   // 4 - rota dinâmica 
   const {id} = useParams()
+
+  const url =  "http://localhost:3000/products/" + id
+
+  const {data: product, loading,error} = useFetch(url)
+
+  console.log(product)
+  // 5 -  carregamento dado individual
   return (
     <>
     <p> Id do produto: {id}</p>
+    {error &&  <p>Ocorreu um erro...</p>}
+    {loading && <p>Carregando...</p>}
+    {product && (
+      <div>
+        <h1>{product.name}</h1>
+        <p>{product.price}</p>
+      </div>
+    )}
     </>
   )
 }
