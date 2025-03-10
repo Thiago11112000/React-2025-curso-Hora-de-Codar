ed4e72d656b5c874fdc8dce6608953148918a578
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-10 10:33:27 -0300

    Nested Routes

--- 7_REACT_ROUTER/reactrouter/src/pages/Product.jsx
+++ 7_REACT_ROUTER/reactrouter/src/pages/Product.jsx
@@ -1,29 +1,31 @@
-import React from "react"
-import { data, useParams} from "react-router-dom"
-import { useFetch } from "../hooks/useFetch"
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
 const Product = () => {
-  // 4 - rota dinâmica 
-  const {id} = useParams()
+  // 4 - rota dinâmica
+  const { id } = useParams();
+  const url = "http://localhost:3000/products/" + id;
+  const { data: product, loading, error } = useFetch(url);
+  console.log(product);
 
-  const url =  "http://localhost:3000/products/" + id
-
-  const {data: product, loading,error} = useFetch(url)
-
-  console.log(product)
-  // 5 -  carregamento dado individual
+  // 5 - carregamento dado individual
   return (
     <>
-    <p> Id do produto: {id}</p>
-    {error &&  <p>Ocorreu um erro...</p>}
-    {loading && <p>Carregando...</p>}
-    {product && (
-      <div>
-        <h1>{product.name}</h1>
-        <p>{product.price}</p>
-      </div>
-    )}
+      <p>Id do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>{product.price}</p>
+          {/* 6 - nested routes */}
+          <Link to={`/products/${product.id}/info`}>Mais informações</Link>
+          
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
