9322a8336bf976ff7988453af3d8c55e709c38bc
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-10 18:05:54 -0300

    Link ativo

--- 7_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
+++ 7_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 // - links com react router
-import { Link } from 'react-router-dom'
+import { Link , NavLink} from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <nav>
-   <Link to="/"> Home</Link>
-   <Link to="/about"> Sobre </Link>
+   {/* <Link to="/"> Home</Link>
+   <Link to="/about"> Sobre </Link> */}
+    
+    <NavLink
+    to=""
+    //className={({ isActive}) => (isActive ? "esta-ativo" : "não-ativo")}
+    >
+      Home
+    </NavLink>
+
+   <NavLink to="/about"> Sobre</NavLink>
     </nav>
   )
 }
